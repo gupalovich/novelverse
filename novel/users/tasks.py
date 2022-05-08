850e7a9e4bce536a3d4f5fd2d5ac60879b6f16d4
Dmitry G <sdprecovery@gmail.com>
2022-05-08 12:29:28 +0300

    init users test_tasks; user tasks/test_tasks disabled for now

--- novel/users/tasks.py
+++ novel/users/tasks.py
@@ -1,30 +1,30 @@
-import traceback
+# import traceback
 
-from celery import states
-from celery.exceptions import Ignore
+# from celery import states
+# from celery.exceptions import Ignore
 # from django.contrib.auth import get_user_model
 
-from config.celery_app import app as celery_app
-from novel.books.utils import save_celery_result
-from .models import Profile
+# from config.celery_app import app as celery_app
+# from novel.books.utils import save_celery_result
+# from .models import Profile
 
 # User = get_user_model()
 
 
-@celery_app.task(bind=True, ignore_result=True)
-def update_users_votes(self):
-    try:
-        profiles = Profile.objects.all()
-        profiles_prem = profiles.filter(premium=True)
-        profiles_nonprem = profiles.filter(premium=False)
-        profiles_prem.update(votes=6)
-        profiles_nonprem.update(votes=3)
-    except Exception as exc:
-        save_celery_result(
-            task_id=self.request.id,
-            task_name=self.name,
-            status=states.FAILURE,
-            result=exc,
-            traceback=traceback.format_exc(),
-        )
-        raise Ignore()
+# @celery_app.task(bind=True, ignore_result=True)
+# def update_users_votes(self):
+#     try:
+#         profiles = Profile.objects.all()
+#         profiles_prem = profiles.filter(premium=True)
+#         profiles_nonprem = profiles.filter(premium=False)
+#         profiles_prem.update(votes=6)
+#         profiles_nonprem.update(votes=3)
+#     except Exception as exc:
+#         save_celery_result(
+#             task_id=self.request.id,
+#             task_name=self.name,
+#             status=states.FAILURE,
+#             result=exc,
+#             traceback=traceback.format_exc(),
+#         )
+#         raise Ignore()
